e9ecedcd410ea2675ee885fc63cb0aa0d4ef03d0
Anil Kumar <anil.bhutale@hedgehog.com>
2023-07-26 05:42:02 +0000

    first commit

---
+++ src/config/constant.js
@@ -0,0 +1,7 @@
+let BACKEND_SERVER = null;
+if (process.env.REACT_APP_BACKEND_SERVER) {
+  BACKEND_SERVER = process.env.REACT_APP_BACKEND_SERVER;
+} else {
+  BACKEND_SERVER = "https://django-api-theta.vercel.app/api/";
+}
+export const API_SERVER = BACKEND_SERVER;
